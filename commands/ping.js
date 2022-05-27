ac8934e6736a3d806aec9c2c04dad59a3d03cc2f
Tevo Saks <whi-tevo@hotmail.com>
2022-05-27 20:21:10 +0300

    test3

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const commandName = 'ping';
 const commandDescription = 'Replies with Pong!';
-const commandContent = 'Pong!';
+const commandContent = 'Pong!!';
 
 module.exports = {
 	data: new SlashCommandBuilder()
